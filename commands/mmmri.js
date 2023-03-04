8606e5393b844f65120d8f225f8394f2de9e9a68
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-03 22:48:06 -0500

    new command

---
+++ commands/mmmri.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('vouchers')
+    .setDescription('The truth about MMMRI'),
+    async execute(interaction, client) {
+        if (err) throw err
+        
+        return interaction.reply({ content: "We are all sitting in the waiting room called life, waiting. Waiting for our appointment with death. So, you pick up the metaphoric magazine and pass the time. Whether it's digital media, drugs, sex, religion, career, whatever floats your boat, we all need something to occupy ourselves with while we wait. We all know how this ends." })
+    }
+}
