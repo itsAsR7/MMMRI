7868221036b8a28264d9c57c26a1981bd9d13748
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-03 22:56:25 -0500

    bit short

--- commands/misc/mmmri.js
+++ commands/misc/mmmri.js
@@ -5,8 +5,7 @@ module.exports = {
     .setName('mmmri')
     .setDescription('The truth about MMMRI'),
     async execute(interaction, client) {
-        if (err) throw err
-        
+    
         return interaction.reply({ content: "We are all sitting in the waiting room called life, waiting. Waiting for our appointment with death. So, you pick up the metaphoric magazine and pass the time. Whether it's digital media, drugs, sex, religion, career, whatever floats your boat, we all need something to occupy ourselves with while we wait. We all know how this ends." })
     }
 }
