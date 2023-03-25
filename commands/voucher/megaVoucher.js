a2bd7d8e17a2a885f30bb7e747a2ce208f2613da
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-25 01:21:55 -0400

    mute paul

--- commands/voucher/megaVoucher.js
+++ commands/voucher/megaVoucher.js
@@ -17,7 +17,7 @@ module.exports = {
         console.log(interaction.options.user)
         const usedBy = `${interaction.member.user.username}#${interaction.member.user.discriminator}`
         const useOn= `${target.username}#${target.discriminator}`
-        member.timeout(2419200000_000)
+        member.timeout(28*24*60*60*1000)
             const embed = new EmbedBuilder()
                 .setColor("Red")
                 .setDescription(`${usedBy} used a voucher on ${useOn} \n${useOn} has been timed out for 28 days \n${usedBy} ${okayEmotes[Math.floor(Math.random()*okayEmotes.length)]}`)
