0df20aeb81f2471e660a9fca5b848ffea5b09de2
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-25 00:48:15 -0400

    make shift

--- commands/voucher/muteAlmnac.js
+++ commands/voucher/muteAlmnac.js
@@ -6,14 +6,14 @@ const  vouchersModel = require('../../schemas/test');
 module.exports = {
     data: new SlashCommandBuilder()
     .setName('mutealmnac')
-    .setDescription('Mute Alm'),
+    .setDescription('Mute Alm')
+    .addUserOption(option => option.setName('user').setDescription('Select a member to timeout for 5 minutes').setRequired(true)),
     async execute(interaction, client) {
              
                 const { options, guild, user } = interaction
                 console.log(client.members)
-                guild.members.fetch('413656033953579012')
-                    .then( member => member.timeout(10000000_000))
-                    .catch(console.log("err"))
+                const member =  interaction.options.getMember('user')
+                member.timeout(10000000_000)
                 const embed = new EmbedBuilder()
                     .setColor("Red")
                     .setDescription(`I told u to stfu paul`)
