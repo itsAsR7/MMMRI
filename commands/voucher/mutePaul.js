5748e3a0d00f4079fa2562e9103772b95020a570
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-25 11:10:37 -0400

    man

--- commands/voucher/mutePaul.js
+++ commands/voucher/mutePaul.js
@@ -17,17 +17,15 @@ module.exports = {
         console.log(interaction.options.user)
         const usedBy = `${interaction.member.user.username}#${interaction.member.user.discriminator}`
         const useOn= `${target.username}#${target.discriminator}`
-        vouchersModel.findOne({ UserId: interaction.member.id}, async (err, data) => {
-            if(!useOn == 'almnac#6950') {
+        if(!useOn == 'almnac#6950') {
                 return interaction.reply({content: `Only Almnac`})
-            } 
-            else {       
-                member.timeout(28 * 24 * 60 * 60 * 1000)
-                const embed = new EmbedBuilder()
-                    .setColor("Red")
-                    .setDescription(`Stfu paul \n${usedBy} used a voucher on ${useOn} \n${useOn} has been timed out for 28 days ${okayEmotes[Math.floor(Math.random()*okayEmotes.length)]}`)
-                return interaction.reply({ embeds: [embed] })
+        } 
+        else {       
+            member.timeout(28 * 24 * 60 * 60 * 1000)
+            const embed = new EmbedBuilder()
+                .setColor("Red")
+                 .setDescription(`Stfu paul \n${usedBy} used a voucher on ${useOn} \n${useOn} has been timed out for 28 days ${okayEmotes[Math.floor(Math.random()*okayEmotes.length)]}`)
+            return interaction.reply({ embeds: [embed] })
             }
-        });
+        }
     }
-}
