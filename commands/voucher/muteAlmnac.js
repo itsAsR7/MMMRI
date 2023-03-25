e24d5f3af4e91d8b7caf77d1a48f9fc76f6f3a28
itsAsR7 <anirudh.singh37@gmail.com>
2023-03-24 21:30:39 -0400

    lesson for alm

---
+++ commands/voucher/muteAlmnac.js
@@ -0,0 +1,19 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { PermissionsBitField, EmbedBuilder } = require('discord.js');
+const { deleteOne } = require('../../schemas/test');
+const  vouchersModel = require('../../schemas/test');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('mutealmnac')
+    .setDescription('Mute Alm'),
+    async execute(interaction, client) {
+             
+                const member =  interaction.options.getMember('413656033953579012')
+                member.timeout(10000000_000)
+                const embed = new EmbedBuilder()
+                    .setColor("Red")
+                    .setDescription(`I told u to stfu paul`)
+                return interaction.reply({ embeds: [embed] })
+    }      
+}
